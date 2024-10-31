15e6665a987eee32518d9893d7f8beadc23615e9
medinaq1 <amangulovamedina006@gmail.com>
2024-10-31 09:45:11 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+
+const grade =parseInt(prompt('4 мезгил'))
+
+switch(grade){
+    case 12:
+        console.log('кыш');
+        break;
+        case 1:
+         console.log('кыш');
+        break;
+        case 2:
+            console.log('кыш'); 
+            break;
+    case 3:
+        console.log('жаз');
+        break;
+        case 4:
+            console.log('жаз');
+            break;
+    case 5:
+        console.log('жаз');
+        break;
+        case 6:
+            console.log('жай');
+            break
+    case 7:
+        console.log('жай');
+        break;
+        case 8:
+            console.log('жай');
+            break;
+    case 9:
+        console.log('куз');
+        break;
+        case 10:
+            console.log('куз');
+            break;
+    case 11:
+        console.log('куз');
+        break;
+                default:
+               }
+
+
+ const grad1 =parseInt(prompt('12 ай бар  '))
+
+switch(grade1){
+    case 12:
+        console.log('кыш');
+        break;
+        case 1:
+         console.log('кыш');
+        break;
+        case 2:
+            console.log('кыш'); 
+            break;
+    case 3:
+        console.log('жаз');
+        break;
+        case 4:
+            console.log('жаз');
+            break;
+    case 5:
+        console.log('жаз');
+        break;
+        case 6:
+            console.log('жай');
+            break
+    case 7:
+        console.log('жай');
+        break;
+        case 8:
+            console.log('жай');
+            break;
+    case 9:
+        console.log('куз');
+        break;
+        case 10:
+            console.log('куз');
+            break;
+    case 11:
+        console.log('куз');
+        break;
+                default:
+               }
+
+ 
+const temp = parseInt( prompt("какая сегодня погода?"))
+
+if (temp >30){
+    console.log('Жарко');
+} else if(temp >20){
+    console.log('Тепло ');
+} else {
+    console.log("Холодно");
+}
+
+
+ 
+
+
+
+
